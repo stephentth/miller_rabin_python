542536bb5bbb68428e1f52dd7c5192a4078a647b
Stephen Truong Trung Hieu <trunghieu.truong.bit@gmail.com>
2019-04-03 10:44:01 +0700

    test: add test case for multiprocess

--- tests/test_miller_rabin.py
+++ tests/test_miller_rabin.py
@@ -1,34 +1,48 @@
-import logging
+import time
 import random
 import unittest
 
-from miller_rabin.miller_rabin import miller_rabin
+from miller_rabin.miller_rabin import miller_rabin, miller_rabin_mutilprocess
 
-DEFAULT_TEST_K = 1
-logging.basicConfig(level=logging.DEBUG)
+DEFAULT_TEST_K = 4
+
+prime_test_cases = [
+    # Trivia case
+    (1, False),
+    (2, True),
+    (3, True),
+    (4, False),
+    (5, True),
+    (13, True),
+    (25, False),
+    # Big number
+    (1548587, True),
+    (1551087, False),
+    (15485867, True),  # 1000001 st prime
+    (15488959, False),
+]
+
 
 class TestMillerRabin(unittest.TestCase):
     def setUp(self):
         random.seed(0)
 
     def test_default(self):
-        test_cases = [
-            # Triva case
-            (1, False),
-            (2, True),
-            (3, True),
-            (4, False),
-            (5, True),
-            (13, True),
-            (25, False),
+        for num, expect in prime_test_cases:
+            print("Run test for", num)
+            start = time.time()
+            self.assertEqual(
+                miller_rabin(num, DEFAULT_TEST_K), expect, f"{num} should be {expect}"
+            )
+            print("Time executed: {:.4f}s".format(time.time() - start))
 
-            # Big number
-            (1548587, True),
-            (1551087, False),
-            (15485867, True),  # 1000001 st prime
-            (15488959, False),
-        ]
-
-        for num, expect in test_cases:
-            print("Run test again", num)
-            self.assertEqual(miller_rabin(num, DEFAULT_TEST_K), expect, f"{num} should be {expect}")
+    def test_multiprocess(self):
+        for num, expect in prime_test_cases:
+            print("Run test for", num)
+            start = time.time()
+            self.assertEqual(
+                miller_rabin_mutilprocess(num, DEFAULT_TEST_K),
+                expect,
+                f"{num} should be {expect}",
+            )
+            print("Time executed: {:.4f}s".format(time.time() - start))
